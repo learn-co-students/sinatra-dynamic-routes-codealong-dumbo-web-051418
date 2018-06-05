51cfb3105c1e71bab8f4ad6c357c0d28d2e9a68f
github username <github email address>
2018-06-04 22:09:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
 
@@ -8,11 +9,15 @@ class App < Sinatra::Base
   end
 
   # This is a sample dynamic route.
-  get "/hello/:name" do
+  get "/goodbye/:name" do
     @user_name = params[:name]
-    "Hello #{@user_name}!"
+    #binding.pry
+      "Goodbye, #{@user_name}."
   end
 
   # Code your final two routes here:
-
+  get '/multiply/:num1/:num2' do
+    product = params[:num1].to_i * params[:num2].to_i
+    "#{product}"
+  end
 end
